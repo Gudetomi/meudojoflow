191a36b5e2b6ab9b1ff5ccb66d48d42fafbe1b73
Gustavo Detomi <gustavodetomi@gmail.com>
2025-07-08 20:48:20 -0300

    Aluno model

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Aluno extends Model
 {
-    //
+   use HasFactory, HasUuids;
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $table = 'turmas';
+    protected $fillable = [
+        'nome_aluno',
+        'user_id',
+        'unidade_id',
+        'data_nascimento',
+        'cpf',
+        'email',
+        'idade',
+        'sexo',
+        'telefone',
+        'endereco',
+        'bairro',
+        'cidade',
+        'cep',
+        'estado',
+        'possui_responsavel',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+        public function unidade()
+    {
+        return $this->belongsTo(Unidade::class);
+    }
 }
